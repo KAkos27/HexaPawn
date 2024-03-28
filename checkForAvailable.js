fe02d20c2f498ccc1037d55f72738350ed22c150
KAkos27 <akoskosztolanyi@gmail.com>
2024-03-28 15:24:04 +0100

    na vegre jatszhato

--- checkForAvailable.js
+++ checkForAvailable.js
@@ -1,27 +1,79 @@
+import giveClassNames from "./giveClassNames.js";
+import pawnClassNames from "./pawnClassNames.js";
 let clickedPawn;
 
 const checkForAvailable = (pawns, pawn, event) => {
   if (!pawn.className.includes("available")) {
-    const reClassIndex =
-      pawn.className === "blackPawn"
-        ? parseInt(pawn.dataset.buttonId) + 3
-        : parseInt(pawn.dataset.buttonId) - 3;
-    pawns.forEach((invisiblePawn) => {
-      if (parseInt(invisiblePawn.dataset.buttonId) === reClassIndex) {
-        const available = document.querySelectorAll(".available");
-        available.forEach((availablePawn) => {
-          if (availablePawn.className.includes("Pawn")) {
-            availablePawn.classList.remove("available");
-          } else {
-            availablePawn.classList.remove("available");
-            availablePawn.classList.add("invisible");
-          }
-        });
-        invisiblePawn.classList.remove("invisible");
-        invisiblePawn.classList.add("available");
+    giveClassNames(pawnClassNames);
+    const pawnId = parseInt(pawn.dataset.buttonId);
+    const pawnIsLeft = pawnId % 3 === 0;
+    const pawnIsRigth = pawnId % 3 === 2;
+    const toMakeAvailable = [];
+    if (pawnIsLeft) {
+      if (pawn.className.includes("black")) {
+        if (pawns[pawnId + 3].className.includes("invisible")) {
+          toMakeAvailable.push(pawnId + 3);
+        }
+        if (pawns[pawnId + 4].className.includes("whitePawn")) {
+          toMakeAvailable.push(pawnId + 4);
+        }
+      } else {
+        if (pawns[pawnId - 3].className.includes("invisible")) {
+          toMakeAvailable.push(pawnId - 3);
+        }
+        if (pawns[pawnId - 2].className.includes("blackPawn")) {
+          toMakeAvailable.push(pawnId - 2);
+        }
       }
-    });
+    } else if (pawnIsRigth) {
+      if (pawn.className.includes("black")) {
+        if (pawns[pawnId + 3].className.includes("invisible")) {
+          toMakeAvailable.push(pawnId + 3);
+        }
+        if (pawns[pawnId + 2].className.includes("whitePawn")) {
+          toMakeAvailable.push(pawnId + 2);
+        }
+      } else {
+        if (pawns[pawnId - 3].className.includes("invisible")) {
+          toMakeAvailable.push(pawnId - 3);
+        }
+        if (pawns[pawnId - 4].className.includes("blackPawn")) {
+          toMakeAvailable.push(pawnId - 4);
+        }
+      }
+    } else {
+      if (pawn.className.includes("black")) {
+        if (pawns[pawnId + 3].className.includes("invisible")) {
+          toMakeAvailable.push(pawnId + 3);
+        }
+        if (pawns[pawnId + 2].className.includes("whitePawn")) {
+          toMakeAvailable.push(pawnId + 2);
+        }
+        if (pawns[pawnId + 4].className.includes("whitePawn")) {
+          toMakeAvailable.push(pawnId + 4);
+        }
+      } else {
+        if (pawns[pawnId - 3].className.includes("invisible")) {
+          toMakeAvailable.push(pawnId - 3);
+        }
+        if (pawns[pawnId - 4].className.includes("blackPawn")) {
+          toMakeAvailable.push(pawnId - 4);
+        }
+        if (pawns[pawnId - 2].className.includes("blackPawn")) {
+          toMakeAvailable.push(pawnId - 2);
+        }
+      }
+    }
+    for (let i = 0; i < toMakeAvailable.length; i++) {
+      const index = toMakeAvailable[i];
+      const currentPawn = pawns[index];
+      if (currentPawn.className.includes("invisible")) {
+        currentPawn.classList.remove("invisible");
+      }
+      currentPawn.classList.add("available");
+    }
   }
+
   clickedPawn = event.target;
 };
 
