b68baeae1e09234bac23f5f45a10ce256f72c3af
KAkos27 <akoskosztolanyi@gmail.com>
2024-03-30 10:55:40 +0100

    main

--- game.js
+++ game.js
@@ -3,19 +3,23 @@ import pawnClassNames from "./pawnClassNames.js";
 import giveClassNames from "./giveClassNames.js";
 import { checkForAvailable } from "./checkForAvailable.js";
 
-generateBoard();
-giveClassNames(pawnClassNames);
+const game = () => {
+  generateBoard();
+  giveClassNames(pawnClassNames);
 
-/* 
-const blackPawns = document.querySelectorAll(".blackPawn");
-blackPawns.forEach((element) => {
-  element.disabled = true;
-  element.disabled = false;
-}); */
+  /* 
+  const blackPawns = document.querySelectorAll(".blackPawn");
+  blackPawns.forEach((element) => {
+    element.disabled = true;
+    element.disabled = false;
+  }); */
 
-const pawns = document.querySelectorAll("button");
-pawns.forEach((pawn) => {
-  pawn.addEventListener("click", (event) => {
-    checkForAvailable(pawns, pawn, event);
+  const pawns = document.querySelectorAll("button");
+  pawns.forEach((pawn) => {
+    pawn.addEventListener("click", (event) => {
+      checkForAvailable(pawns, pawn, event);
+    });
   });
-});
+};
+
+export default game;
